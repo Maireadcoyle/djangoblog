11f958a1a992266ef42532a59d819add23b85bc4
mairead white <maireadtrade@gmail.com>
2025-01-20 18:26:48 +0000

    add a secret key to envy.py and settings.py and add to config var in heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n1&#s)tj*8tuk1w_xk%(1c*-d=d1lpit9#db4@7)*s=9$s(v3v'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
