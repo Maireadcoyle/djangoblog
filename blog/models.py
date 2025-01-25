1834ae4faaa4181b3e1365ee6ceda601a7c4a7f6
mairead white <maireadtrade@gmail.com>
2025-01-25 18:23:21 +0000

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -4,19 +4,42 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 #create models here
-class Post(models.Model):
+
+
+#createupdatemodel stops you from having to repeat code in different models
+class CreateUpdateModel(models.Model):
+    creation_date = models.DateTimeField(auto_now_add=True)
+    Update_date = models.DateTimeField(auto_now=True)
+#abstract=true will make sure the code above dosnt create a table
+    class Meta:
+        abstract = True
+
+
+     
+
+class Post(CreateUpdateModel):
     # post_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
-    creation_date = models.DateTimeField(auto_now_add=True)
-    Update_date = models.DateTimeField(auto_now=True)
+   # creation_date = models.DateTimeField(auto_now_add=True) no need for these two lines now due to createupdatemodel parent
+   # Update_date = models.DateTimeField(auto_now=True)
     is_draft = models.BooleanField(default=True)
     is_featured = models.BooleanField(default=False)
+    hero_image = models.ImageField(upload_to="post", default="post/sample.jpg")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
-)
-     excerpt = models.TextField(blank=True)
+        )
+    excerpt = models.TextField(blank=True)
+
+    class Comment(CreateUpdateModel):
+        text = models.TextField()
+        post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    # on delete=models.casecade when we delete the post the comment is also deleted
+        author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+        )
+        is_comment_approved = models.BooleanField(default=False)
      
     def __str__(self):
         return self.title
