ff9ace1354bf39af4a72a2ff07b99f5b19ab17f4
mairead white <maireadtrade@gmail.com>
2025-01-28 21:41:02 +0000

    create django generic list view and modify to suit creation_date rather than status

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-STATUS = ((0, "Draft"), (1, "Published"))
+# STATUS = ((0, "Draft"), (1, "Published"))
 
 #create models here
 
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
+from django.utils import timezone
 from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    model = Post
+    queryset = Post.objects.filter(creation_date__lte=timezone.now()).order_by('-creation_date')
+    template_name = "post_list.html"
+#    model = Post
 
     
 def home(request):
